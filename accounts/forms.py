9feae09e3cc93a69e5015080b99e509453021e96
ferlandalex <2063726@csfoy.ca>
2023-03-02 09:41:11 -0500

    register user functionnalities and home settup

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,3 +8,10 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
+    def clean(self):
+        clean_data = super(UserForm, self).clean()
+        password = clean_data.get('password')
+        confirm_password = clean_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError('Password does not match')
